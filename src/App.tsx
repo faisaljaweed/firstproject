d11b5a00a1ccfa38f6c2b63b61b7a6166db464ec
faisal Jawed <faisaljawed2000@gmail.com>
2024-04-24 09:49:59 +0500

    add paragraph

--- src/App.tsx
+++ src/App.tsx
@@ -8,8 +8,9 @@ function App() {
     <>
       <div>
       <h1 className="text-3xl font-bold underline">
-      Hello world!
+      Hello world! 
     </h1>
+    <p>Muhammad Faisal Jawed x</p>
       </div>
     </>
   )
